fcbbd4cb18c1ccb1177ba49d75fd14f28a62a234
jayjaysilva <jethrojundelossantos@gmail.com>
2015-12-10 21:56:53 -0500

    can now scrape 8.5k unique items

---
+++ crawlallforyou/crawlallforyou/items.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class CrawlallforyouItem(scrapy.Item):
+    urls = scrapy.Field()
+    title = scrapy.Field()
+    retailer_sku_code = scrapy.Field()
+    desc = scrapy.Field()
+    offer = scrapy.Field()
+    out_of_stock = scrapy.Field()
+    old_price = scrapy.Field()
+    #model = scrapy.Field()
+    #mpn = scrapy.Field()
+    sku = scrapy.Field()
+    #upc = scrapy.Field()
+    #ean = scrapy.Field()
+    #currency = scrapy.Field()
+    image = scrapy.Field()
+    price = scrapy.Field()
+    crawl_time = scrapy.Field()
+    #current_price = scrapy.Field()
+    #primary_image_url = scrapy.Field()
+    categories = scrapy.Field()
